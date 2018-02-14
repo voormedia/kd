570ecb7c91367b71de2acf691deb65192587c0a8
Rolf Timmermans <rolftimmermans@voormedia.com>
2018-02-14 18:53:43 +0100

    Bump.

--- cmd/root.go
+++ cmd/root.go
@@ -10,7 +10,7 @@ var log = util.NewLogger("kd")
 var cmdRoot = &cobra.Command{
 	Use:     "kd",
 	Short:   "Build and deploy apps to k8s cluster",
-	Version: "1.1.5",
+	Version: "1.1.6",
 }
 
 var verbose bool
